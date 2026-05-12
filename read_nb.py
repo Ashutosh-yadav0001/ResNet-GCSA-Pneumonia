ad75486f21de97d3dfb1be7ac2bddebce18a7be1
Ashutosh Yadav <ashuyadav277001@gmail.com>
2026-05-13 00:01:32 +0530

    Add PneumoFusionNet: 1ch grayscale ResNet50 + GCSA + DSC

--- read_nb.py
+++
@@ -1,6 +0,0 @@
-import json
-nb = json.load(open('PneumoFusionNet_Xray_Implementation.ipynb', 'r', encoding='utf-8'))
-print(f"Total cells: {len(nb['cells'])}")
-for i, c in enumerate(nb['cells']):
-    src = ''.join(c['source'])
-    print(f"Cell {i} ({c['cell_type']}): {repr(src[:100])}")
